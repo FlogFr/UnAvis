a6f08e879400b77703982ad7d4ff694140e6f530
aRkadeFR <contact@arkade.info>
2015-02-16 23:13:51 +0100

    checking logger

--- unavis/views/common.py
+++ unavis/views/common.py
@@ -7,3 +7,7 @@ logger = logging.getLogger('django.views')
 
 class HomeView(generic_views.TemplateView):
     template_name = 'core/home.html'
+
+    def get(self, *args, **kwargs):
+        logger.info('hitting home view')
+        return super(HomeView, self).get(*args, **kwargs)
