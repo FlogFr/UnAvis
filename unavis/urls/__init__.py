b51db27e13b9589bb77967813fb278122517e25e
aRkadeFR <contact@arkade.info>
2015-03-01 19:23:37 +0100

    L10n & i18n ok

--- unavis/urls/__init__.py
+++ unavis/urls/__init__.py
@@ -1,13 +1,18 @@
 from django.conf.urls import patterns, url, include
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 
+from django.views import i18n as i18n_views
 from unavis import views
 
-urlpatterns = patterns(
+urlpatterns = i18n_patterns(
     '',
 
     url(r'^$',
         views.HomeView.as_view(), name="home"),
+
+    url(r'^language/set/$',
+        i18n_views.set_language, name="set_language"),
 
     url(r'^category/list/$',
         views.CategoryList.as_view(), name="category_list"),
--- unavis/views/common.py
+++ unavis/views/common.py
@@ -7,3 +7,6 @@ logger = logging.getLogger('django.views')
 
 class HomeView(generic_views.TemplateView):
     template_name = 'core/home.html'
+
+    def get(self, request, *args, **kwargs):
+        return super(HomeView, self).get(request, *args, **kwargs)
